a3982927934a20efcef3211e23df3add92b60374
juckphai <llsvuslnw@gmail.com>
2025-11-11 13:46:50 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // กำหนดชื่อ Cache
-const staticCacheName = 'account-app-static-v159';
-const dynamicCacheName = 'account-app-dynamic-v158';
+const staticCacheName = 'account-app-static-v149';
+const dynamicCacheName = 'account-app-dynamic-v148';
 
 // ไฟล์ที่ต้องการ cache
 const assets = [
